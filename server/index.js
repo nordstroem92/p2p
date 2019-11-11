bea91ec629ed71a9997a88a71789d436a6428bbd
eichwald <eichwald6@hotmail.com>
2019-11-11 20:37:29 +0100

    test sqlite databse on heroku

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static(__dirname + 'images'));
-app.use(express.static(__dirname + 'js'));
 
 const port = process.env.PORT || 5000;
 
@@ -35,16 +34,27 @@ let db = new sqlite3.Database('./test.db', (err) => {
 db.run('CREATE TABLE IF NOT EXISTS langs(name text)');
 
 db.run(`INSERT INTO langs(name) VALUES(?)`, ['C'], function(err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    // get the last insert id
-    console.log(`A row has been inserted with rowid ${this.lastID}`);
-});
+      if (err) {
+        return console.log(err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
 
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
+let sql = `SELECT DISTINCT Name name FROM langs
+           ORDER BY name`;
+
+var minutes = 5, the_interval = minutes * 1 * 1000;
+
+setInterval(function() {
+  db.all(sql, [], (err, rows) => {
+  if (err) {  
+    throw err;
   }
-  console.log('Close the database connection.');
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
 });
+  // do your stuff here
+}, the_interval);
+
