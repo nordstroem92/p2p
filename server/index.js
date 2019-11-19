7f2b2dd3bf585b55ff2a28387f4863dbf72c8725
nordstroem92 <nordstroem92@gmail.com>
2019-11-19 14:48:30 +0100

    jaja

--- server/index.js
+++ server/index.js
@@ -36,21 +36,21 @@ app.get('/', function (req, res) {
 
 //app.ws('/echo', function(ws, req) {
 s.on('connection',function(ws,req){
-/******* when server receives messsage from client trigger function with argument message *****/
-ws.on('message',function(message){
-console.log("Received: "+message);
-s.clients.forEach(function(client){ //broadcast incoming message to all clients (s.clients)
-if(client!=ws && client.readyState ){ //except to the same client (ws) that sent this message
-client.send("broadcast: " +message);
-}
-});
-// ws.send("From Server only to sender: "+ message); //send to client where message is from
-});
-ws.on('close', function(){
-console.log("lost one client");
-});
-//ws.send("new client connected");
-console.log("new client connected");
+  /******* when server receives messsage from client trigger function with argument message *****/
+  ws.on('message',function(message){
+  console.log("Received: "+message);
+    s.clients.forEach(function(client){ //broadcast incoming message to all clients (s.clients)
+      if(client!=ws && client.readyState ){ //except to the same client (ws) that sent this message
+        client.send("broadcast: " +message);
+      }
+    });
+    // ws.send("From Server only to sender: "+ message); //send to client where message is from
+  });
+  ws.on('close', function(){
+    console.log("lost one client");
+  });
+  //ws.send("new client connected");
+  console.log("new client connected");
 });
 
 app.use('/watchdog', function (req, res, next) {
