bf5c6b554ad2c0a2e201400f937251afea287be7
nordstroem92 <nordstroem92@gmail.com>
2019-11-19 21:21:05 +0100

    ws added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -20,3 +22,21 @@ const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
+
+function originIsAllowed(origin) {
+  return true;
+}
+
+
+const WebSocketServer = require('ws').Server
+const wss = new WebSocketServer({ port: 8081 });
+wss.on('connection', ((ws) => {
+ws.on('message', (message) => {
+console.log(`received: ${message}`);
+});
+
+ws.on('end', () => {
+console.log('Connection ended...');
+});
+ws.send('Hello Client');
+}));
